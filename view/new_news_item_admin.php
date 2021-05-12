2de217d77ea6ba7bd9219e18463141d4aeaf4c2d
Andrei <gerasimenko_andrei@bk.ru>
2021-05-12 15:58:49 +0700

    file for correct operation of adding news

---
+++ view/new_news_item_admin.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <!--<link rel="stylesheet" type="text/css" href="../css/style.css">-->
+   <title>Добавление новости</title>
+</head>
+<body>
+   <div class="container">
+   <form method="post" action="news_panel.php?action=add">
+   <label>
+   Заголовок
+   <input type="text" name="title" value="" class="form-item" autofocus required>
+   </label>
+   <label>
+   Дата
+   <input type="date" name="date" value="" class="form-item" required>
+   </label>
+   <label>
+   Содержание
+   <textarea class="form-item" name="content" required></textarea>
+   </label>
+   <input type="submit" value="Сохранить" class="bnt">
+   </form>
+   </div>
+</body>
+</html>
