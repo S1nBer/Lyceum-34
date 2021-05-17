9e65dc78cf20eb25fb5484e9b0d303bc785ed166
Andrei <gerasimenko_andrei@bk.ru>
2021-05-17 22:51:07 +0700

    +section ped_team

--- components/ped_team.php
+++ components/ped_team.php
@@ -1,26 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" type="text/css" href="../css/style.css">
-   <title>Педколлектив</title>
-</head>
-<body>
 <?php
-require_once '../components/navbar.php';
-?>
-<div class="body">
-<div class="cur_events">
+require_once '../database.php';
+require_once '../models/ped_team.php';
 
-</div>
-<?php
-require_once '../components/ads.php';
+$link = db_connect();
+$categories = categories_all($link);
+
+if(isset($_POST['category'])){
+   $category = $_POST['category'];
+   $teachers = teachers_all($link, $category);}
+
+include("../components/teachers.php");
+
 ?>
-</div>
-<?php
-require_once '../components/footer.php';
-?>
-</body>
-</html>
