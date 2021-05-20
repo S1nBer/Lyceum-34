88ee6c1be2fb0268b41c798cc9db9ef3d06dd75d
Andrei <gerasimenko_andrei@bk.ru>
2021-05-20 13:12:04 +0700

    wrap img in text editor

--- view/ads_item.php
+++ view/ads_item.php
@@ -5,6 +5,9 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" type="text/css" href="../css/style.css">
+   <link rel="stylesheet" type="text/css" href="../css/fancybox.jquery.css">
+   <script type="text/javascript" src="../scripts/jquery-3.5.1.min.js"></script>
+   <script type="text/javascript" src="../scripts/jquery.fancybox.js"></script>
    <title>Новости Лицея №34</title>
 </head>
 <body>
@@ -24,5 +27,11 @@
    <?php
    require_once '../components/footer.php';
    ?>
+   <script>
+   let article = document.getElementsByClassName('article');
+   $('.article img').each(function () {
+    $(this).wrap("<a data-fancybox href='" + this.src + "' rel='shadowbox'></a>");
+   });
+   </script>
 </body>
 </html>
