346bbbe8a26d9af4657a1bec948313d987adef16
Andrei <gerasimenko_andrei@bk.ru>
2021-05-26 12:54:44 +0700

    version for the visually impaired

--- components/navbar.php
+++ components/navbar.php
@@ -5,7 +5,8 @@
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <link rel="stylesheet" type="text/css" href="../css/style.css">
-   <script type="text/javascript" src="../scripts/jquery-3.5.1.min.js"></script>
+   <link rel="stylesheet" href="../css/bvi.min.css" type="text/css">
+   <link rel="stylesheet" href="../css/bvi.css" type="text/css">
    <title>Лицей №34</title>
 </head>
 <body>
@@ -22,19 +23,19 @@
          ?>
          <li><a target="_blank" href="https://cabinet.ruobr.ru/login/?next=/child/studies/mark_table/">Электронный дневник</a></li>
       <li><div class="button">
-         <button class="version">
-            <table class="button">
-            <tr>
-               <td id="col11"><img class="eye" src="../img/eye.png"></td>
-               <td id="col12"><div class="stick"></div></td>
-               <td id="col13"><div class="Version">
-                  Версия для слабовидящих
-               </div></td>
-            </tr>
-            </table>
-         </button>
+         <!--<button>-->
+         <a href="#" class="bvi-open" title="Версия сайта для слабовидящих"><i class="bvi-icon bvi-eye bvi-2x"></i>
+            <img class="eye" src="../img/eye.png"></a>
+               <!--<div class="stick"></div>
+               <p class="version">Версия для слабовидящих</p>
+         </button>-->
       </div></li>
       </ul>
    </div>
+<script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script src="../scripts/responsivevoice.min.js"></script>
+<script src="../scripts/js.cookie.js"></script>
+<script src="../scripts/bvi-init.js"></script>
+<script src="../scripts/bvi.min.js"></script>
 </body>
 </html>
